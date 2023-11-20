4d02b019c0fb5ca4febac13d551e09ba24789340
Alex Gaudi <itehnoviking@gmail.com>
2023-11-20 16:23:42 +0300

    Add project files.

---
+++ LinkShorteningSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinkShorteningSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
